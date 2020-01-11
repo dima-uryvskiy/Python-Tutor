ac52334224ed479fee492c9f716ce6e5f76fbab3
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-11 23:19:17 +0300

    Add solution theme 11 task 2-3

---
+++ Theme 11/Task 2.py
@@ -0,0 +1,21 @@
+"""
+Условие
+Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову.
+Все слова в словаре различны.
+Для слова из словаря, записанного в последней строке, определите его синоним.
+"""
+
+
+dict_word = dict()
+
+for value in range(int(input())):
+    first_word, second_word = input().split()
+    dict_word[first_word] = second_word
+
+word = input()
+
+for key, value in dict_word.items():
+    if value == word:
+        print(key)
+    elif key == word:
+        print(value)
